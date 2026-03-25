687374ba1d6dc044cfd02d51c11fda8c2e8255d4
DankaMarci <marciemail7@gmail.com>
2026-03-25 17:06:45 +0100

    Function refactor

--- ticketing-frontend/src/app/page.tsx
+++ ticketing-frontend/src/app/page.tsx
@@ -6,19 +6,13 @@ import { useState } from "react";
 export default function Home() {
   const [counter, setCounter] = useState(0)
 
-  const handleClick = () => {
-    setCounter(counter + 1)
-  }
-
   console.log("render")
-
-  
 
   return (
     <div className="min-h-screen bg-white flex flex-col flex-1 items-center justify-center">
       <button 
         className="flex flex-row font-sans text-3xl text-black bg-slate-400 rounded-full p-20 justify-center items-center hover:scale-105"
-        onClick={handleClick}
+        onClick={() => setCounter(counter + 1)}
       >
         <Image src={"/Kir-Dev.png"} width={200} height={200} alt="Kir-Dev" className="mr-4"/>
         {counter}++++
