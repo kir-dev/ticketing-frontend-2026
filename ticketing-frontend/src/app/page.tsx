74a4d6588abebd531f1792d7984525fe9e8324ba
DankaMarci <marciemail7@gmail.com>
2026-03-25 17:03:52 +0100

    Change bg colors

--- ticketing-frontend/src/app/page.tsx
+++ ticketing-frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black flex flex-col flex-1 items-center justify-center">
-      <div className="flex flex-row font-sans text-3xl text-black bg-white rounded-full p-20 justify-center items-center">
+    <div className="min-h-screen bg-white flex flex-col flex-1 items-center justify-center">
+      <div className="flex flex-row font-sans text-3xl text-black bg-slate-400 rounded-full p-20 justify-center items-center">
         <Image src={"/Kir-Dev.png"} width={200} height={200} alt="Kir-Dev" className="mr-4"/>
         Greetings from Kir-Dev!
       </div>
